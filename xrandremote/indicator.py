15a71556cf82a67a31c6f7df6a3daa757eaefeab
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-09-08 22:39:17 -0500

    flesh out the indicator a bit more

--- xrandremote/indicator.py
+++ xrandremote/indicator.py
@@ -1,5 +1,5 @@
+from xrandremote.util import logger, get_icon, PKG_NAME
 import signal
-from xrandremote.util import logger
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
@@ -7,39 +7,79 @@ gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk  # noqa
 from gi.repository import AppIndicator3 as appindicator  # noqa
 
+LOG = logger(__name__)
 
-APPINDICATOR_ID = 'xrandremote'
+
+def create_callback(function, *default_args, **default_kwargs):
+    def instantiate_callback(*args, **kwargs):
+        default_args.extend(args)
+        default_kwargs.update(**kwargs)
+        return function(*default_args, **default_kwargs)
+
+    return instantiate_callback
+
+
+class IndicatorMenu(gtk.Menu):
+    def __init__(self, callback, refresh):
+        super(IndicatorMenu, self).__init__()
+        self._callback = callback
+        self._refresh = refresh
+        self.entries = []
+        self.update()
+        self.static_setup()
+        self._refresh(None)
+        self.update()
+
+    def static_setup(self):
+        self.append(gtk.SeparatorMenuItem.new())
+
+        refresh = gtk.MenuItem("Refresh")
+        refresh.connect("activate", self._refresh)
+        self.append(refresh)
+
+        settings = gtk.MenuItem("Settings")
+        # settings.connect("activate", window.on_show_origin)
+        self.append(settings)
+
+        about = gtk.MenuItem("About")
+        # about.connect("activate", window.on_show_author)
+        self.append(about)
+        self.show_all()
+        LOG.debug('showing all')
+
+    def update(self, entries=[]):
+        self.clear_entries()
+        if not entries:
+            none = gtk.MenuItem("None")
+            none.set_sensitive(False)
+            self.add_entry(none)
+        for entry in entries:
+            toggle_item = gtk.CheckMenuItem.new_with_label(entry)
+            toggle_item.connect(
+                'activate', create_callback(self._callback, entry=entry))
+            self.add_entry(toggle_item)
+        self.show_all()
+
+    def add_entry(self, entry):
+        self.insert(entry, len(self.entries))
+        self.entries.append(entry)
+
+    def clear_entries(self):
+        for entry in self.entries:
+            self.remove(entry)
+        self.entries = []
 
 
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, '/home/lutostag/src/xrandremote/icons/scalable/xrandremote-indicator.svg', appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(
+        PKG_NAME, get_icon('xrandremote-indicator.svg'),
+        appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    menu = gtk.Menu()
-    none = gtk.MenuItem("None")
-    none.set_sensitive(False)
-    menu.append(none)
-
-    toggle_item = gtk.CheckMenuItem.new_with_label('Enabled')
-    menu.append(toggle_item)
-
-    menu.append(gtk.SeparatorMenuItem.new())
-
-    refresh = gtk.MenuItem("Refresh")
-    # refresh.connect("activate", window.open_file)
-    menu.append(refresh)
-
-    settings = gtk.MenuItem("Settings")
-    # settings.connect("activate", window.on_show_origin)
-    menu.append(settings)
-
-    about = gtk.MenuItem("About")
-    # about.connect("activate", window.on_show_author)
-    menu.append(about)
-    menu.show_all()
-
+    menu = IndicatorMenu(callback=None, refresh=lambda x: True)
     indicator.set_menu(menu)
     gtk.main()
+
 
 if __name__ == "__main__":
     main()
--- xrandremote/util.py
+++ xrandremote/util.py
@@ -1,10 +1,16 @@
 import os
 import logging
 
+PKG_NAME = 'xrandremote'
+
 
 def logger(filename):
     top_dir = os.path.dirname(os.path.abspath(__file__))
     relative = os.path.relpath(os.path.abspath(filename), start=top_dir)
-    location = os.path.join('xrandremote', relative)
+    location = os.path.join(PKG_NAME, relative)
     module_name = location.replace(os.sep, '.').rsplit('.', 1)[0]
     return logging.getLogger(module_name)
+
+
+def get_icon(filename):
+    return '/home/lutostag/src/xrandremote/icons/scalable/xrandremote-indicator.svg'
